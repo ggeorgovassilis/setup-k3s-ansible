c39926c55ccfdee3ca916cd2819eb20f02bce61d
george georgovassilis <georgovassilisg@unisystems.gr>
2022-11-01 09:18:44 +0000

    fixed filename

--- setup-elastic-cluster.sh
+++ setup-elastic-cluster.sh
@@ -3,10 +3,8 @@
 PATH_TO_SSH_KEY="sshkey.private"
 SSH_USER="root"
 
-# Absolute path to this script, e.g. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in, thus /home/user/bin
-BASE=$(dirname "$SCRIPT")
+PATH_TO_SCRIPT=$(readlink -f "$0")
+base=$(dirname "$PATH_TO_SCRIPT")
 
 cd "$BASE"
 
@@ -14,5 +12,5 @@ function ans() {
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --inventory-file "hosts" --private-key "$PATH_TO_SSH_KEY" -u SSH_USER "$@"
 }
 
-ans setup-k3s-ansible/common.playbook
-ans elastic.playbook
+ans setup-k3s.playbook
+ans setup-elastic.playbook
