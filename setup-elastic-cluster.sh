85d11a39e81049f702ca908ebec7c1054042d1f3
george georgovassilis <georgovassilisg@unisystems.gr>
2022-11-01 09:08:51 +0000

    refactoring and ignoring ssh key

--- setup-elastic-cluster.sh
+++ setup-elastic-cluster.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-base="/home/george/Desktop/elastic"
+
+PATH_TO_SSH_KEY="sshkey.private"
+SSH_USER="root"
+
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+BASE=$(dirname "$SCRIPT")
+
+cd "$BASE"
 
 function ans() {
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --inventory-file "$base/hosts" --private-key "$base/sshkey.private" -u root "$@"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --inventory-file "hosts" --private-key "$PATH_TO_SSH_KEY" -u SSH_USER "$@"
 }
 
 ans setup-k3s-ansible/common.playbook
